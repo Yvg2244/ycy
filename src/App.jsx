9c63c1c6866a114ffd1ac1e8046a895af0517a03
Yvg2244 <yvgupta24@gmail.com>
2023-06-22 20:37:26 +0530

    ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+"use client";
 import { ListItemSuffix, ThemeProvider } from "@material-tailwind/react";
-import { BsBookFill, BsFillJournalBookmarkFill } from "react-icons/bs";
+import {
+  BsBookFill,
+  BsFillJournalBookmarkFill,
+  BsFilter,
+  BsArrowLeftCircle,
+  BsArrowRightCircle,
+} from "react-icons/bs";
 import { GrNotes } from "react-icons/gr";
+
 import { LuHistory } from "react-icons/lu";
 import { MdOutlineRecentActors } from "react-icons/md";
 import { RiRestartLine } from "react-icons/ri";
@@ -13,6 +21,7 @@ import React from "react";
 import {
   Collapse,
   Avatar,
+  Drawer,
   Button,
   ListItemPrefix,
   Textarea,
@@ -28,8 +37,9 @@ import {
   CardBody,
 } from "@material-tailwind/react";
 function App() {
-  const [open, setOpen] = useState(false);
-  const toggleOpen = () => setOpen((cur) => !cur);
+  const [showSidebar, setShowSidebar] = useState(true);
+  const [showTab, setShowTab] = useState(false);
+
   const [selected, setSelected] = React.useState(1);
   const setSelectedItem = (value) => setSelected(value);
   const data = [
@@ -56,103 +66,122 @@ function App() {
   ];
 
   return (
-
-      <div className="w-[100vw] grid grid-cols-3 gap-4 font-poppins tracking-wide p-8 h-[100vh] bg-[#e9ecef] text-[#212529]">
-        <div className="col-span-1 bg-white rounded-md border-[#ced4da] border-[1px] py-5">
-          <div className="w-full flex justify-center">
-            <Button
-              onClick={toggleOpen}
-              className="font-bold font-poppins flex gap-5 items-center text-xl bg-[#212529] text-[#e9ecef]"
-            >
-              My Library
-              <span>
-                <IoIosArrowDropdown className="font-[1000]" />
-              </span>
-            </Button>
-          </div>
-          <Collapse open={open}>
-            <Card className="my-4 mx-auto w-8/12 border-[#ced4da] border-[1px]">
-              <CardBody className="p-0">
-                <List>
-                  <ListItem>
-                    <ListItemPrefix>
-                      <BsBookFill />
-                    </ListItemPrefix>
-                    <div>
-                      <Typography variant="h6" color="blue-gray">
-                        Books
-                      </Typography>
-                    </div>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemPrefix>
-                      <GrNotes />
-                    </ListItemPrefix>
-                    <div>
-                      <Typography variant="h6" color="blue-gray">
-                        Notes
-                      </Typography>
-                    </div>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemPrefix>
-                      <FaRegNewspaper />
-                    </ListItemPrefix>
-                    <div>
-                      <Typography variant="h6" color="blue-gray">
-                        Pre-paper
-                      </Typography>
-                    </div>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemPrefix>
-                      <MdOutlineRecentActors />
-                    </ListItemPrefix>
-                    <div>
-                      <Typography variant="h6" color="blue-gray">
-                        Study List
-                      </Typography>
-                    </div>
-                  </ListItem>{" "}
-                  <ListItem>
-                    <ListItemPrefix>
-                      <LuHistory />
-                    </ListItemPrefix>
-                    <div>
-                      <Typography variant="h6" color="blue-gray">
-                        History
-                      </Typography>
-                    </div>
-                  </ListItem>{" "}
-                  <ListItem>
-                    <ListItemPrefix>
-                      <RiRestartLine />
-                    </ListItemPrefix>
-                    <div>
-                      <Typography variant="h6" color="blue-gray">
-                        Recent Read
-                      </Typography>
-                    </div>
-                  </ListItem>
-                </List>
-              </CardBody>
-            </Card>
-          </Collapse>
+    <div className="w-[100vw] grid grid-cols-3 gap-4 font-poppins tracking-wide p-8 h-[100vh] bg-[#e9ecef] text-[#212529]">
+      <div className="col-span-1 bg-white rounded-md border-[#ced4da] border-[1px] py-5">
+        <div className="w-full flex justify-center">
+          <Button
+            onClick={() => setShowSidebar(!showSidebar)}
+            className="font-bold font-poppins flex gap-5 items-center text-xl bg-[#FF7400] text-[#e9ecef]"
+          >
+            My Library
+            <span>
+              {showSidebar ? (
+                <BsArrowLeftCircle className="font-[1000]" />
+              ) : (
+                <BsArrowRightCircle className="font-[1000]" />
+              )}
+            </span>
+          </Button>
         </div>
-        <div className="col-span-2 bg-white rounded-md border-[#ced4da] border-[1px] py-5 gap-5 flex flex-col">
-          <div className=" px-5 py-1">
-            <div className="w-full gap-2 flex justify-center items-center">
-              <textarea
-                name=""
-                id=""
-                className="w-[90%] py-2 px-5 font-4  border-[#ced4da] border-[2px] rounded-lg"
-                rows="1"
-                placeholder="Seacrh"
-              ></textarea>
-              <AiOutlineSearch className="text-[1.5rem]" />
-            </div>
+        <div
+          className={`ease-in-out duration-300 ${
+            showSidebar ? "translate-x-0 z-[10]" : "translate-x-[-100%] z-[-10]"
+          }`}
+        >
+          <Card className="my-4 mx-auto w-8/12 ">
+            <CardBody className="p-0">
+              <List>
+                <ListItem>
+                  <ListItemPrefix>
+                    <BsBookFill className="text-[#FF7400] " />
+                  </ListItemPrefix>
+                  <div>
+                    <Typography variant="h6" color="blue-gray">
+                      Books
+                    </Typography>
+                  </div>
+                </ListItem>
+                <ListItem>
+                  <ListItemPrefix>
+                    <GrNotes className="text-[#FF7400]" />
+                  </ListItemPrefix>
+                  <div>
+                    <Typography variant="h6" color="blue-gray">
+                      Notes
+                    </Typography>
+                  </div>
+                </ListItem>
+                <ListItem>
+                  <ListItemPrefix>
+                    <FaRegNewspaper className="text-[#FF7400] " />
+                  </ListItemPrefix>
+                  <div>
+                    <Typography variant="h6" color="blue-gray">
+                      Pre-paper
+                    </Typography>
+                  </div>
+                </ListItem>
+                <ListItem>
+                  <ListItemPrefix>
+                    <MdOutlineRecentActors className="text-[#FF7400] " />
+                  </ListItemPrefix>
+                  <div>
+                    <Typography variant="h6" color="blue-gray">
+                      Study List
+                    </Typography>
+                  </div>
+                </ListItem>{" "}
+                <ListItem>
+                  <ListItemPrefix>
+                    <LuHistory className="text-[#FF7400] " />
+                  </ListItemPrefix>
+                  <div>
+                    <Typography variant="h6" color="blue-gray">
+                      History
+                    </Typography>
+                  </div>
+                </ListItem>{" "}
+                <ListItem>
+                  <ListItemPrefix>
+                    <RiRestartLine className="text-[#FF7400] " />
+                  </ListItemPrefix>
+                  <div>
+                    <Typography variant="h6" color="blue-gray">
+                      Recent Read
+                    </Typography>
+                  </div>
+                </ListItem>
+                <div className="w-[100%]flex items-center">
+                  <button className="font-normal px-6 py-2 rounded-lg font-poppins text-md w-auto flex gap-5 items-center text-md bg-[#FF7400] text-[#e9ecef]">
+                    Request Notes
+                  </button>
+                </div>
+              </List>
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+      <div className="col-span-2 bg-white rounded-md border-[#ced4da] border-[1px] py-5 gap-5 flex flex-col">
+        <div className=" px-5 py-1">
+          <div className="w-full gap-2 flex justify-center items-center">
+            <textarea
+              name=""
+              id=""
+              className="w-[90%] py-2 px-5 font-4  border-[#ced4da] border-[2px] rounded-lg"
+              rows="1"
+              placeholder="Seacrh"
+            ></textarea>
+            <AiOutlineSearch className="text-[#FF7400] text-[1.5rem]" />
+            <BsFilter
+              className="text-[#FF7400] text-[1.5rem]"
+              onClick={() => {
+                setShowTab(!showTab);
+              }}
+            />
           </div>
-          <div className="w-full p-5 flex justify-center ">
+        </div>
+        <div className="w-full p-5 flex justify-center ">
+          {showTab && (
             <Tabs value="material" className="w-[90%] font-poppins ">
               <TabsHeader className="">
                 {data.map(({ label, value }) => (
@@ -169,10 +198,10 @@ function App() {
                 ))}
               </TabsBody>
             </Tabs>
-          </div>
+          )}
         </div>
       </div>
-   
+    </div>
   );
 }
 
